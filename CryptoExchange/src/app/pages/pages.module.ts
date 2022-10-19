34785da7844fff7719bcb9be20150253c7c206a0
David Lazarte <dilazarte@gmail.com>
2022-10-19 19:18:42 -0300

    Se modifica modulo de paginas

--- CryptoExchange/src/app/pages/pages.module.ts
+++ CryptoExchange/src/app/pages/pages.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
 import { MovimientosComponent } from './movimientos/movimientos.component';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
   declarations: [
     QuienesSomosComponent,
-    MovimientosComponent
+    MovimientosComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     QuienesSomosComponent,
-    MovimientosComponent
+    MovimientosComponent,
+    LoginComponent
   ]
 })
 export class PagesModule { }
